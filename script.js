ea88ef70c4d43640020a2b1a606e433c2f9a733c
Alphby48 <hazerdmobile@gmail.com>
2023-10-27 00:43:54 +0700

    local

--- script.js
+++ script.js
@@ -6,6 +6,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
   let arrBuku = [];
 
+  const periksaPenyimpanan = localStorage.getItem("BOOKSHELF_CLIENT_SIDE");
+  if (periksaPenyimpanan) {
+    arrBuku = JSON.parse(periksaPenyimpanan);
+  }
+
+  function simpanBukuDiLocalStorage() {
+    localStorage.setItem("BOOKSHELF_CLIENT_SIDE", JSON.stringify(arrBuku));
+  }
+
   formInput.addEventListener("submit", function (e) {
     e.preventDefault();
     const judul = document.getElementById("inputBookTitle").value;
@@ -25,6 +34,7 @@ document.addEventListener("DOMContentLoaded", function () {
         isComplete: cek,
       };
       arrBuku.push(buku);
+      simpanBukuDiLocalStorage();
 
       perbaruiInput();
 
@@ -58,7 +68,7 @@ document.addEventListener("DOMContentLoaded", function () {
     author.textContent = "Penulis : " + buku.author;
 
     const year = document.createElement("p");
-    year.textContent = "Penulis : " + buku.year;
+    year.textContent = "Tahun : " + buku.year;
 
     const action = document.createElement("div");
     action.classList.add("action");
@@ -69,7 +79,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     let tglBtn;
     if (buku.isComplete) {
-      tglBtn = createButton("Belum Selesai Dibaca", "yellow", function () {
+      tglBtn = createButton("Belum Selesai Dibaca", "green", function () {
         toggleIsComplete(buku.id);
       });
     } else {
@@ -99,6 +109,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const index = arrBuku.findIndex((buku) => buku.id === id);
     if (index !== -1) {
       arrBuku.splice(index, 1);
+      simpanBukuDiLocalStorage();
       perbaruiInput();
     }
   }
@@ -107,7 +118,37 @@ document.addEventListener("DOMContentLoaded", function () {
     const index = arrBuku.findIndex((buku) => buku.id === id);
     if (index !== -1) {
       arrBuku[index].isComplete = !arrBuku[index].isComplete;
+      simpanBukuDiLocalStorage();
       perbaruiInput();
+    }
+  }
+
+  const cariBuku = document.getElementById("searchBook");
+  const cariJudul = document.querySelector("#searchBookTitle");
+  cariBuku.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const nilaiJudul = cariJudul.value.toLowerCase().trim();
+
+    const carihasil = arrBuku.filter((buku) => {
+      return (
+        buku.title.toLowerCase().includes(nilaiJudul) ||
+        buku.author.toLowerCase().includes(nilaiJudul) ||
+        buku.year.toString().includes(nilaiJudul)
+      );
+    });
+    perbaruiPencarian(carihasil);
+  });
+
+  function perbaruiPencarian(hasil) {
+    belumSelesai.innerHTML = "";
+    sudahSelesai.innerHTML = "";
+    for (const buku of hasil) {
+      const itemBuku = buatTampil(buku);
+      if (buku.isComplete) {
+        sudahSelesai.appendChild(itemBuku);
+      } else {
+        belumSelesai.appendChild(itemBuku);
+      }
     }
   }
 
